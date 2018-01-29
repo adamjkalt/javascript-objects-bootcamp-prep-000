5e58578dc4e7ecf7ae6e5598e5a473ff8e7254e8
Adam Kalt <adam.kalt.1@gmail.com>
2018-01-29 19:58:44 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,6 @@
 var playlist = {artistName: "songTitle)"};
 
-function updatePlaylist (playlist, artistName, songTitle) {playlist[songTitle] = wholePlaylist
+function updatePlaylist (playlist, artistName, songTitle) {playlist[songTitle] = ()
+
+return playlist
+}
