ad1539a968aa1b5df2ded6bcdd90e424ec74f95e
Adam Kalt <adam.kalt.1@gmail.com>
2018-01-29 20:26:06 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {artistName: "songTitle)"};
 
-function updatePlaylist (playlist, artistName, songTitle) {artistName[songTitle] = ();
+function updatePlaylist (playlist, artistName, songTitle){
 
 return playlist;
 }
